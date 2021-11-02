16789d848d8ba78be7f391f765b17130052fb166
samuelayo <ayoogundipe2005@gmail.com>
2021-11-02 15:56:30 +0100

    use array

--- src/controllers/interactiveController.js
+++ src/controllers/interactiveController.js
@@ -1,41 +1,46 @@
-const generateFavouriteHobbiesDropDown = () => ({
-  blocks: [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: 'What are your favorite hobbies?',
-      },
-      accessory: {
-        type: 'multi_static_select',
-        placeholder: {
-          type: 'plain_text',
-          text: 'Select an item',
-          emoji: true,
-        },
-        options: [
-          {
-            text: {
-              type: 'plain_text',
-              text: 'Choice 1',
-              emoji: true,
-            },
-            value: 'value-0',
-          },
-          {
-            text: {
-              type: 'plain_text',
-              text: 'Choice 2',
-              emoji: true,
-            },
-            value: 'value-1',
-          },
-        ],
-        action_id: 'create_feedback_final_step',
-      },
+const generateDropDown = require('../utils/generateDropdown');
+
+const generateFavouriteHobbiesDropDown = () => ([
+  {
+    type: 'section',
+    block_id: 'section678',
+    text: {
+      type: 'mrkdwn',
+      text: 'Pick items from the list',
     },
-  ],
-});
+    accessory: {
+      action_id: 'text1234',
+      type: 'multi_static_select',
+      placeholder: {
+        type: 'plain_text',
+        text: 'Select items',
+      },
+      options: [
+        {
+          text: {
+            type: 'plain_text',
+            text: '*this is plain_text text*',
+          },
+          value: 'value-0',
+        },
+        {
+          text: {
+            type: 'plain_text',
+            text: '*this is plain_text text*',
+          },
+          value: 'value-1',
+        },
+        {
+          text: {
+            type: 'plain_text',
+            text: '*this is plain_text text*',
+          },
+          value: 'value-2',
+        },
+      ],
+    },
+  },
+]);
 const moodSelectionResponse = (payload) => {
   let selectedResponse = payload && payload.actions;
   selectedResponse = selectedResponse && selectedResponse[0];
@@ -55,18 +60,18 @@ const processInteraction = (req, res) => {
   const { body } = req;
   let { payload } = body;
   try {
-    console.log('starting intraction')
+    console.log('starting intraction');
     payload = JSON.parse(payload);
     const functionType = payload && payload.callback_id;
     if (!interactiveMap[functionType]) {
-        console.log('oops, no func')
+      console.log('oops, no func');
       return res.status(400).json({ ok: false, message: 'unknown interaction' });
     }
     const result = interactiveMap[functionType](payload);
-    console.log('finishing intraction', result)
+    console.log('finishing intraction', result);
     return res.status(200).json(result);
   } catch (error) {
-    console.log('erroring intraction', error)
+    console.log('erroring intraction', error);
     return res.status(400).json({ ok: false, message: error && error.message });
   }
 };
