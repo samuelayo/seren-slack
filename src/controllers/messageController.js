e65c1edd9da90ed2632638173ea41d7d504d8294
samuelayo <ayoogundipe2005@gmail.com>
2021-11-02 12:18:01 +0100

    add request signing

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -12,6 +12,7 @@ const verifyMessage = (req, res, next) => {
   const time = Math.floor(new Date().getTime() / 1000);
 
   if (Math.abs(time - timestamp) > 300) {
+      console.log("late message", time, timestamp)
     return res.status(400).json({ ok: false, message: 'Message appears to have been sent more than 5 mins ago' });
   }
 
@@ -29,6 +30,7 @@ const verifyMessage = (req, res, next) => {
     //
     next();
   }
+  console.log("verification failed")
   return res.status(400).send('Verification failed');
 };
 
