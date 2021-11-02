f1eeb9ef2d41092db766fa338cd93052cb12ddb7
samuelayo <ayoogundipe2005@gmail.com>
2021-11-02 12:37:54 +0100

    remove qs

--- src/config/express.js
+++ src/config/express.js
@@ -16,8 +16,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 
+const rawBodySaver = function (req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || 'utf8');
+  }
+};
+
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, verify: rawBodySaver }));
 
 module.exports = app;
--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -4,33 +4,28 @@ const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 
 const verifyMessage = (req, res, next) => {
   // message comes in
-  const slackRequest = JSON.stringify(req.body);
   const slackSignature = req.headers['x-slack-signature'];
+  const [version, slackHash] = slackSignature.split('=');
   const timestamp = req.headers['X-Slack-Request-Timestamp'];
   // convert current time from milliseconds to seconds
   const time = Math.floor(new Date().getTime() / 1000);
 
   if (Math.abs(time - timestamp) > 300) {
-      console.log("late message", time, timestamp);
+    console.log('late message', time, timestamp);
     return res.status(400).json({ ok: false, message: 'Message appears to have been sent more than 5 mins ago' });
   }
 
-  const sigBasestring = `v0:${timestamp}:${slackRequest}`;
+  const sigBasestring = `${version}:${timestamp}:${req.rawBody}`;
 
-  const mySignature = `v0=${
-    crypto.createHmac('sha256', slackSigningSecret)
-      .update(sigBasestring, 'utf8')
-      .digest('hex')}`;
-      console.log(slackSigningSecret, mySignature, slackSignature)
-  if (crypto.timingSafeEqual(
-    Buffer.from(mySignature, 'utf8'),
-    Buffer.from(slackSignature, 'utf8'),
-  )
-  ) {
-    //
-    next();
+  const hmac = crypto.createHmac('sha256', slackSigningSecret);
+  hmac.update(sigBasestring);
+  const derivedHash = hmac.digest('hex');
+  if (crypto.timingSafeEqual(Buffer.from(slackHash), Buffer.from(derivedHash))) {
+    return next();
   }
-  console.log("verification failed")
+  console.log(slackSigningSecret, slackHash, derivedHash);
+
+  console.log('verification failed');
   return res.status(400).send('Verification failed');
 };
 
